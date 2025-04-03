851b0192c38528a247b46bb65b155a80138fd522
Enoki <enokiun@gmail.com>
2025-04-03 23:24:05 +0300

    feat: potentially implement rest of the thing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, ToTokens};
 use syn::{
-    parse_macro_input, parse_quote, punctuated::Punctuated, spanned::Spanned, Error, Expr, ItemFn,
-    LitStr, Meta, Stmt, Token,
+    parse_macro_input, parse_quote, punctuated::Punctuated, spanned::Spanned, Error, Expr, ExprIf,
+    ItemFn, LitStr, Meta, Stmt, Token,
 };
 
 macro_rules! get_attrs {
@@ -24,21 +24,43 @@ macro_rules! get_attrs {
     };
 }
 
-#[proc_macro_attribute]
-pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut item = parse_macro_input!(item as ItemFn);
-    let mut benched: Vec<String> = vec![];
+fn handle_if(benched: &mut Vec<String>, if_expr: &mut ExprIf) -> Result<(), TokenStream> {
+    handle_macro(benched, &mut if_expr.then_branch.stmts)?;
+    if let Some(else_branch) = &mut if_expr.else_branch {
+        match &mut *else_branch.1 {
+            Expr::Block(expr_block) => handle_macro(benched, &mut expr_block.block.stmts)?,
+            Expr::If(if_expr) => handle_if(benched, if_expr)?,
+            _ => unreachable!(),
+        }
+    }
+    Ok(())
+}
 
-    let _: Vec<&Stmt> = item
-        .block
-        .stmts
-        .iter()
-        .inspect(|s| println!("{:#?}", s))
-        .collect();
-
-    let mut stmt_idx = 1;
-    while stmt_idx < item.block.stmts.len() {
-        let attrs = match &item.block.stmts[stmt_idx] {
+fn handle_macro(benched: &mut Vec<String>, stmts: &mut Vec<Stmt>) -> Result<(), TokenStream> {
+    let mut stmt_idx = 0;
+    while stmt_idx < stmts.len() {
+        if let Stmt::Expr(expr, _) = &mut stmts[stmt_idx] {
+            match expr {
+                Expr::Block(expr_block) => handle_macro(benched, &mut expr_block.block.stmts)?,
+                Expr::ForLoop(expr_for_loop) => {
+                    handle_macro(benched, &mut expr_for_loop.body.stmts)?
+                }
+                Expr::If(expr_if) => handle_if(benched, expr_if)?,
+                Expr::Loop(expr_loop) => handle_macro(benched, &mut expr_loop.body.stmts)?,
+                Expr::Match(expr_match) => {
+                    for arm in &mut expr_match.arms {
+                        if let Expr::Block(block) = &mut *arm.body {
+                            handle_macro(benched, &mut block.block.stmts)?
+                        }
+                    }
+                }
+                Expr::While(expr_while) => handle_macro(benched, &mut expr_while.body.stmts)?,
+                Expr::Async(expr_async) => handle_macro(benched, &mut expr_async.block.stmts)?,
+                Expr::Unsafe(expr_unsafe) => handle_macro(benched, &mut expr_unsafe.block.stmts)?,
+                _ => {}
+            }
+        }
+        let attrs = match &stmts[stmt_idx] {
             Stmt::Local(local) => local.attrs.clone(),
             Stmt::Expr(expr, _) => get_attrs!(
                 expr, Array, Assign, Async, Await, Binary, Block, Break, Call, Cast, Closure,
@@ -56,35 +78,38 @@ pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
                     match list.parse_args_with(Punctuated::<LitStr, Token![,]>::parse_terminated) {
                         Ok(list) => {
                             if list.len() != 1 {
-                                return Error::new(list.span(), "Invalid usage of `bench` macro. Only one argument is supported.")
+                                return Err(Error::new(list.span(), "Invalid usage of `bench` macro. Only one argument is supported.")
                                     .to_compile_error()
-                                    .into();
+                                    .into());
                             }
                             let name = list.first().unwrap().clone().value();
                             let var_ident = format_ident!("_bench_{}_time", name);
                             let instant_ident = format_ident!("_bench_{}_instant", name);
-                            benched.push(name);
+                            if !benched.contains(&name) {
+                                benched.push(name);
+                            }
                             bench_macro_idx = Some(i);
-                            item.block.stmts.insert(
+                            stmts.insert(
                                 stmt_idx,
                                 parse_quote! {let #instant_ident = std::time::Instant::now();},
                             );
-                            item.block
-                                .stmts
-                                .insert(stmt_idx + 2, parse_quote! {let #var_ident = #var_ident + #instant_ident.elapsed().as_micros();});
+                            stmts.insert(
+                                stmt_idx + 2,
+                                parse_quote! {#var_ident +=  #instant_ident.elapsed().as_micros();},
+                            );
                             stmt_idx += 2;
                         }
-                        Err(err) => return err.to_compile_error().into(),
+                        Err(err) => return Err(err.to_compile_error().into()),
                     }
                 } else {
-                    return Error::new(attr.span(), "Invalid usage of `bench` macro")
+                    return Err(Error::new(attr.span(), "Invalid usage of `bench` macro")
                         .to_compile_error()
-                        .into();
+                        .into());
                 }
             }
         }
         if let Some(i) = bench_macro_idx {
-            let attrs = match &mut item.block.stmts[stmt_idx - 1] {
+            let attrs = match &mut stmts[stmt_idx - 1] {
                 Stmt::Local(local) => &mut local.attrs,
                 Stmt::Expr(expr, _) => get_attrs!(
                     mut expr,
@@ -135,12 +160,23 @@ pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
         stmt_idx += 1;
     }
+    Ok(())
+}
+
+#[proc_macro_attribute]
+pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut item = parse_macro_input!(item as ItemFn);
+    let mut benched: Vec<String> = vec![];
+
+    if let Err(err) = handle_macro(&mut benched, &mut item.block.stmts) {
+        return err;
+    };
 
     for name in benched {
         let var_ident = format_ident!("_bench_{}_time", name);
         item.block
             .stmts
-            .insert(0, parse_quote! {let #var_ident: u128 = 0;});
+            .insert(0, parse_quote! {let mut #var_ident: u128 = 0;});
         item.block.stmts.push(
             parse_quote! {log::debug!("Benchmark for {}: {} microseconds", #name, #var_ident);},
         );
